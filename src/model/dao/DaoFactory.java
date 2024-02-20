302b3dc81a552d372a9500426e77c52d9ca75266
AlexFerreira10 <correaferreiraalex@gmail.com>
2024-02-20 16:18:43 -0300

    I implemented the program domain

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	//Expoem o tipo da interface, mas internamente instancia uma implementação
+	//Programa nao conhece a implementação somente a interface
+	//Injeçao de dependencia sem explicitar a implemnetação
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+}
