06778455509ea321b974e409c652fef0a8e11d31
AlexFerreira10 <correaferreiraalex@gmail.com>
2024-02-20 22:17:05 -0300

    I implemented the SellerDaoJDBC functions

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,12 +13,39 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Cars");
-		System.out.println(obj);
+		Scanner sc = new Scanner(System.in);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		System.out.println("=== Test 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-
+		
+		System.out.println("\n=== Test 2: seller findById ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		list.forEach(System.out :: println);
+		
+		System.out.println("\n=== Test 3: seller findAll ===");
+		list = sellerDao.findAll();
+		list.forEach(System.out :: println);
+		
+		System.out.println("\n=== Test 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller insert ===");
+		seller = sellerDao.findById(8);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update!");
+		
+		System.out.println("\n=== Test 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
-
 }
